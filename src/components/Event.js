87e360c2ff96ce9e1354feaa38453a19f4a55f17
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-03-03 13:51:51 -0600

    Update Event.js

--- src/components/Event.js
+++ src/components/Event.js
@@ -2,27 +2,20 @@ import { useState } from "react";
 
 const Event = ({ event }) => {
     const [showDetails, setShowDetails] = useState(false);
-
-    const toggleDetails = () => {
-        setShowDetails(!showDetails);
-    };
-
     return (
-        <li className="event" key={event.id}>
-            <h2>{event.summary}</h2>
-            <p>{event.created}</p>
+        <li className="event">
+            <h3>{event.summary}</h3>
             <p>{event.location}</p>
-            <button className='showDetailsButton' onClick={toggleDetails}>
-                {showDetails ? 'Hide Details' : 'Show Details'}
-            </button>
-            {showDetails && (
-                <div className="details">
-                    <h3>Event Details</h3>
-                    <p>{event.description}</p>
-                </div>
-            )}
+            <p>{(new Date(event.created)).toUTCString()}</p>
+            {showDetails ?
+                <p className="details">{event.description}</p> :
+                null
+            }
+            <button className="details-btn" onClick={() => {
+                showDetails ? setShowDetails(false) : setShowDetails(true)
+            }}>{showDetails ? "hide details" : "show details"}</button>
         </li>
-    );
-};
+    )
+}
 
 export default Event;
