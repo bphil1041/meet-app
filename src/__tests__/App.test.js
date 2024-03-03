a24596bb0cecd9d3c5bb2f9d1b6d32597965beb7
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-03-03 16:45:35 -0600

    Update App.test.js

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,19 +1,21 @@
-// src/__tests__/App.test.js
-
 import { render } from '@testing-library/react';
 import App from '../App';
 
 describe('<App /> component', () => {
     let AppDOM;
     beforeEach(() => {
-        AppDOM = render(<App />).container.firstChild;
-    })
+        AppDOM = render(<App />).container;
+    });
 
     test('renders list of events', () => {
         expect(AppDOM.querySelector('#event-list')).toBeInTheDocument();
     });
 
-    test('render CitySearch', () => {
+    test('renders CitySearch', () => {
         expect(AppDOM.querySelector('#city-search')).toBeInTheDocument();
+    });
+
+    test('renders NumberOfEvents component', () => {
+        expect(AppDOM.querySelector('.numberOfEvents')).toBeInTheDocument();
     });
 });
