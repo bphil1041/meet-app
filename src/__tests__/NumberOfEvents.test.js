d845cfd038a0d53966f5a7505fd6f68b26f0d492
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-03-03 16:45:37 -0600

    Create NumberOfEvents.test.js

---
+++ src/__tests__/NumberOfEvents.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import NumberOfEvents from '../components/NumberOfEvents';
+
+test('contains an element with role spinbutton', () => {
+    render(<NumberOfEvents />);
+    expect(screen.getByRole('spinbutton')).toBeInTheDocument();
+});
+
+test('default value of the input field is 32', () => {
+    render(<NumberOfEvents />);
+    const inputElement = screen.getByRole('spinbutton');
+    expect(inputElement).toHaveValue(32);
+});
+
+test('value of the input field changes when user types in it', async () => {
+    render(<NumberOfEvents />);
+    const inputElement = screen.getByRole('spinbutton');
+    fireEvent.change(inputElement, { target: { value: '10' } });
+    expect(inputElement).toHaveValue(10);
+});
