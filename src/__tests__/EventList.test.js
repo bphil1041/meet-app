6415a1980dc3c89c883a186ae498c78636d92aac
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-03-04 09:05:13 -0600

    Update EventList.test.js

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -17,4 +17,8 @@ describe('<EventList /> component', () => {
         EventListComponent.rerender(<EventList events={allEvents} />);
         expect(EventListComponent.getAllByRole("listitem")).toHaveLength(allEvents.length);
     });
-}); 
+});
+
+describe('<EventList /> integration', () => {
+
+});
