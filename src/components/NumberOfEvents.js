14e1656e3c44d54c5589ab0fb7a4915700d47646
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-03-08 08:33:56 -0600

    Update NumberOfEvents.js

--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -6,13 +6,17 @@ class NumberOfEvents extends React.Component {
     };
 
     handleInputChanged = (event) => {
-        const value = event.target.value;
+        const value = parseInt(event.target.value);
         this.setState({ eventsNumber: value });
+        console.log('Current number of events:', value);
+        this.props.setCurrentNOE(value); // Add this line to update the parent component's state
     };
+
+
 
     render() {
         return (
-            <div className="numberOfEvents">
+            <div className="numberOfEvents" id="number-of-events" >
                 <label htmlFor="numberOfEventsInput">Number of events:</label>
                 <input
                     type="number"
