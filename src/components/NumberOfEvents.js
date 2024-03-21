cab273e515cbe0e42952661e322f334a4d818768
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-03-21 08:40:08 -0500

    Update NumberOfEvents.js

--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -7,22 +7,29 @@ class NumberOfEvents extends React.Component {
 
     handleInputChanged = (event) => {
         const value = parseInt(event.target.value);
-        this.setState({ eventsNumber: value });
-        console.log('Current number of events:', value);
-        this.props.setCurrentNOE(value); // Add this line to update the parent component's state
-    };
-
+        this.setState({ eventsNumber: value }, () => {
+            console.log('Current number of events:', value);
+            this.props.setCurrentNOE(value); // Update the parent component's state
+            let infoText;
+            if (isNaN(value) || value <= 0) {
+                infoText = "Only positive numbers are allowed";
+            } else {
+                infoText = "";
+            }
+            this.props.setErrorAlert(infoText); // Update the error alert message
+        });
+    }
 
 
     render() {
         return (
-            <div className="numberOfEvents" id="number-of-events" >
+            <div className="numberOfEvents" id="number-of-events">
                 <label htmlFor="numberOfEventsInput">Number of events:</label>
                 <input
                     type="number"
                     id="numberOfEventsInput"
                     name="numberOfEvents"
-                    defaultValue={this.state.eventsNumber}
+                    value={this.state.eventsNumber}
                     min={1}
                     max={100}
                     onChange={this.handleInputChanged}
