9e1dccc00fbf50c6d71727e19ffcce1b7426fc72
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-03-21 08:40:03 -0500

    Update NumberOfEvents.test.js

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -15,10 +15,21 @@ test('default value of the input field is 32', () => {
 
 test('value of the input field changes when user types in it', async () => {
     let currentNOE = 32;
-    const setCurrentNOE = (value) => { currentNOE = value };
-    render(<NumberOfEvents setCurrentNOE={setCurrentNOE} />);
+    const setCurrentNOE = jest.fn().mockImplementation((value) => {
+        currentNOE = value;
+    });
+
+    const mockSetErrorAlert = jest.fn();
+
+    render(<NumberOfEvents setCurrentNOE={setCurrentNOE} setErrorAlert={mockSetErrorAlert} />);
     const inputElement = screen.getByRole('spinbutton');
+
     fireEvent.change(inputElement, { target: { value: '10' } });
+
     expect(inputElement).toHaveValue(10);
     expect(currentNOE).toBe(10);
+    expect(setCurrentNOE).toHaveBeenCalledTimes(1);
+    expect(setCurrentNOE).toHaveBeenCalledWith(10);
+    expect(mockSetErrorAlert).toHaveBeenCalledTimes(1);
+    expect(mockSetErrorAlert).toHaveBeenCalledWith('');
 });
